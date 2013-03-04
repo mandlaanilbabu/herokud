ad48fbe38ec9d65a05a43801b4c67a7704a83b24
anilbabu <mandla.anilbabu@gmail.com>
2013-03-04 12:35:11 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Herocuanil::Application.config.secret_token = '2d83b23edd3e6a769a78d3e6ccf290a11299ce0c0cb10465101d375283914031d6777a8655540f08d7094f63ef2436c4a46b74e5b66062c1ff0932bf5b1ab2db'
